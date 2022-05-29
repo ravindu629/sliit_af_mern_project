d37cf77ed0ea3c8098d36a34584f3c2be8eee50a
Sandaru Divyantha <79299187+sandarudivyantha@users.noreply.github.com>
2022-05-29 13:31:22 +0530

    Student Page Sucessfully created and subject model deleted

--- client/src/components/students/StudentMainPage.jsx
+++ client/src/components/students/StudentMainPage.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import "../App.css";
+import Welcomeimage from "../images/WelcomeInStudentMainPage.jpg";
 
-function StudentMenu() {
-  return <div></div>;
+function StudentMainPage() {
+  return (  
+  
+  <div className="sysMenu">
+
+    <br/>  
+    <div  className="WlcomeImg">
+      <img className="img-std-WelcomeInMainPage" src={Welcomeimage} />
+    </div>
+    <br/>
+
+  <div className="navItem">
+    <a href="/AFpage">
+      <button type="button" className="stMainPageBtn">
+          <pre>Application Frameworks - SE3040                                               🎓</pre>
+      </button>
+    </a>
+  </div>
+  <hr/>
+
+  <div className="navItem">
+    <a href="/DSpage">
+      <button type="button" className="stMainPageBtn">
+          <pre>Distributed Systems - SE3020                                                  🎓</pre>
+          <i class="glyphicon glyphicon-user"></i>
+      </button>
+    </a>
+  </div>
+  <hr/>
+
+  <div className="navItem">
+    <a href="/SApage">
+      <button type="button" className="stMainPageBtn">
+          <pre>Software Architecture - SE3030                                                🎓</pre>
+      </button>
+    </a>
+  </div>
+  <hr/>
+  
+</div>);
 }
 
-export default StudentMenu;
+export default StudentMainPage;
--- models/Subject.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
-
-const subjectDetails = new Schema({
-  subjectName: String,
-  subjectID: String
-});
-
-module.exports = Student = mongoose.model("Subject", subjectDetails);
