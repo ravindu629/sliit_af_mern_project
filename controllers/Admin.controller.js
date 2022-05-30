a0e0c577cbe38929d3324cd6fead87ccd1bb3012
ravindu629 <ravinduwijerathne629@gmail.com>
2022-05-30 14:37:27 +0530

    create admin model, controller and route in the backend

---
+++ controllers/Admin.controller.js
@@ -0,0 +1,118 @@
+const Admin = require("../models/Admin.model");
+
+const addAdmin = (req, res) => {
+  const { fName, lName, adminId, nic, phoneNumber, email, password } = req.body;
+
+  const admin = new Admin({
+    fName,
+    lName,
+    adminId,
+    nic,
+    phoneNumber,
+    email,
+    password,
+  });
+
+  admin
+    .save()
+    .then((createdAdmin) => {
+      res.json(createdAdmin);
+    })
+    .catch((error) => {
+      res.status(400).json(error);
+    });
+};
+
+const getAdmins = async (req, res) => {
+  try {
+    const admins = await Admin.find();
+    res.json(admins);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const getAdmin = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const admin = await Admin.findById(userId);
+    res.json(admin);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const updateAdmin = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const user = await Admin.findById(userId);
+
+    if (!user) {
+      return res.status(404).json("There is no admin to update");
+    }
+
+    const { fName, lName, adminId, nic, phoneNumber, email, password } =
+      req.body;
+
+    const updatedUser = await Admin.findByIdAndUpdate(userId, {
+      fName,
+      lName,
+      adminId,
+      nic,
+      phoneNumber,
+      email,
+      password,
+    });
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+};
+
+const removeAdmin = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const user = await Admin.findById(userId);
+
+    if (!user) {
+      return res.status(404).json("There is no admin to remove");
+    }
+
+    const removedUser = await Admin.findByIdAndDelete(userId);
+    res.status(200).json(removedUser);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+};
+
+const validateAdmin = async (req, res) => {
+  const admId = req.body.adminId;
+  const pass = req.body.password;
+
+  try {
+    const foundUser = await Admin.findOne({ adminId: admId });
+
+    if (!foundUser) {
+      return res.status(404).json("invalid user");
+    } else if (foundUser.password === pass) {
+      return res.status(200).json(true);
+    } else {
+      return res.status(404).json("incorrect password");
+    }
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+module.exports = {
+  addAdmin,
+  getAdmins,
+  getAdmin,
+  updateAdmin,
+  removeAdmin,
+  validateAdmin,
+};
