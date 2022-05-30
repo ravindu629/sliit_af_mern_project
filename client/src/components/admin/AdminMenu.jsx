a4082f252999dbe51f09ea479504b7d8c460c540
ravindu629 <ravinduwijerathne629@gmail.com>
2022-05-31 02:06:33 +0530

    Add styling to the AdminLogin

--- client/src/components/admin/AdminMenu.jsx
+++ client/src/components/admin/AdminMenu.jsx
@@ -1,53 +1,44 @@
 import React from "react";
 import "../App.css";
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
 
 export default function AdminMenu() {
   return (
-    <div style={{ paddingLeft: "400px", paddingTop: "80px" }}>
-      <h2 style={{ fontWeight: "bold" }}>Admin Menu</h2>
-      <div className="navItem">
-        <a href="/manageUsers">
-          <button
-            type="button"
-            className="btn btn-secondary menuBtn"
-            style={{ width: "35%", fontSize: "140%", marginTop: "20px" }}
-          >
-            Manage Users
-          </button>
-        </a>
-      </div>
-      <div className="navItem">
-        <a href="/addAdmin">
-          <button
-            type="button"
-            className="btn btn-secondary menuBtn"
-            style={{ width: "35%", fontSize: "140%", marginTop: "20px" }}
-          >
-            Add a new Admin
-          </button>
-        </a>
-      </div>
-      <div className="navItem">
-        <a href="">
-          <button
-            type="button"
-            className="btn btn-secondary menuBtn"
-            style={{ width: "35%", fontSize: "140%", marginTop: "20px" }}
-          >
-            Create submission types
-          </button>
-        </a>
-      </div>
-      <div className="navItem">
-        <a href="">
-          <button
-            type="button"
-            className="btn btn-secondary menuBtn"
-            style={{ width: "35%", fontSize: "140%", marginTop: "20px" }}
-          >
-            Create marking schemes
-          </button>
-        </a>
+    <div className="adminMenu">
+      <div className="admMenu">
+        <h2 style={{ fontWeight: "bold" }}>Admin Menu</h2>
+        <div className="navItem">
+          <a href="/manageUsers">
+            <button type="button" className="btn btn-secondary menuBtn">
+              <ArrowCircleRightIcon />
+              &nbsp;&nbsp;&nbsp;&nbsp; Manage Users
+            </button>
+          </a>
+        </div>
+        <div className="navItem">
+          <a href="/addAdmin">
+            <button type="button" className="btn btn-secondary menuBtn">
+              <ArrowCircleRightIcon />
+              &nbsp;&nbsp;&nbsp;&nbsp; Add a new Admin
+            </button>
+          </a>
+        </div>
+        <div className="navItem">
+          <a href="">
+            <button type="button" className="btn btn-secondary menuBtn">
+              <ArrowCircleRightIcon />
+              &nbsp;&nbsp;&nbsp;&nbsp; Create submission types
+            </button>
+          </a>
+        </div>
+        <div className="navItem">
+          <a href="">
+            <button type="button" className="btn btn-secondary menuBtn">
+              <ArrowCircleRightIcon />
+              &nbsp;&nbsp;&nbsp;&nbsp; Create marking schemes
+            </button>
+          </a>
+        </div>
       </div>
     </div>
   );
