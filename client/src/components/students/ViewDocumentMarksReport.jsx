e851e99af5fc06a237fe7a45a2efc012438bd94f
Sandaru Divyantha <79299187+sandarudivyantha@users.noreply.github.com>
2022-06-02 21:02:45 +0530

    ViewDocument marks report/ student prograss chat ctrated and connected to mongo DB chart

---
+++ client/src/components/students/ViewDocumentMarksReport.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+
+export default function ViewMarksNavPage() {
+    return (
+        <div><br/><br/>
+        <h2 align="center">Students Prograss</h2>
+            <div style={{ marginLeft: 290, fontSize: 20 }}>
+                <iframe title="myFrame001" width="900" height="490" src="https://charts.mongodb.com/charts-project-0-fthfb/embed/dashboards?id=37f2d895-5425-4a2d-8419-a7302dfefbff&theme=light&autoRefresh=true&maxDataAge=10&showTitleAndDesc=false&scalingWidth=scale&scalingHeight=scale"></iframe>
+            </div>
+        </div>
+
+    );
+}
